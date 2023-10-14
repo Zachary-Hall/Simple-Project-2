05b5ebd97dd363df56d300fd898f97a148960419
Zach <Zachary.hall3@g.austincc.edu>
2023-10-14 13:09:23 -0500

    added method functions for assignNode (private), exists, getNode, addNode and addHead

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,3 +1,118 @@
 /*
- * your comment header here
+ * COSC 2436
+ * Zachary Hall
+ * Linked Lists Assignment 
+ * 10/4/23
  */
+
+#include "linkedlist.h"
+LinkedList::LinkedList(){
+	head = NULL;
+}
+
+	bool LinkedList::addHead(Node* current, Node* newNode){
+		bool added = false;
+		
+		if (current->data.id == newNode->data.id){
+			added = false;
+		}
+		else if (current->data.id > newNode->data.id){
+			if(!current->prev){
+				head->prev = newNode;
+				newNode->prev = NULL;
+				newNode->next = head;
+				head = newNode;
+				added = true;
+			} else {
+				newNode->next = current;
+				newNode->prev = current->prev;
+				current->prev->next = newNode;
+				current->prev - newNode;
+				added = true;
+			}
+		}
+		else if (current->next){
+			added = addHead(current->next, newNode);
+		} else {
+			newNode->next = NULL;
+			newNode->prev = current;
+			current->next = newNode;
+			added = true;
+		}
+		return added;
+	}
+
+	
+	bool LinkedList::addNode(int id, std::string* data) {
+        bool working = false;
+				
+				if(id > 0 && *data != ""){
+				
+        if (!head) {
+        		Node* newNode = new Node;
+        		assignNode(id, data, newNode);
+        		head = newNode;
+            newNode->next = NULL;
+            head->prev = NULL;
+            working = true;
+        } else {
+        		Node *newNode = new Node;
+        		assignNode(id, data, newNode);
+        		working = addHead(head, newNode);
+        }
+    }
+        return working;
+  }
+
+    
+    bool LinkedList::getNode(int id, Data* data) {
+        bool nodeAquired = false;
+        if (head != NULL) {
+          Node* current = head;
+          while (current != NULL){
+          	if (current->data.id == id){
+          		data->id = current->data.id;
+          		data->data = current->data.data;
+          		nodeAquired = true;
+						}
+						current = current->next;
+				}
+		}
+		return nodeAquired;	
+  }
+    
+
+	void LinkedList::assignNode(int id, string* data, Node* newNode){
+		Data* newData = new Data{id, *data};
+		newNode->data = *newData;
+	}
+    
+    int LinkedList::getCount() {
+       int count = 0;
+       Node* current = head;
+       while (current != NULL){
+       	count++;
+       	current = current->next;
+			 }
+			 return count;
+    }
+    
+     bool LinkedList::exists(int id) {
+        bool does_exist = false;
+        if (head != NULL) {
+        		Node* current = head;
+        	while(current != NULL){
+            if (current->data.id == id) {
+                does_exist = true;
+            }
+            current = current->next;
+        }
+    }
+      return does_exist;
+}
+    
+
+LinkedList::~LinkedList() {
+	clearList();
+}
+	
