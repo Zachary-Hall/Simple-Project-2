7367e31bdcb06cb67fa8e4ec87ac0b0045b032d1
Zach <Zachary.hall3@g.austincc.edu>
2023-10-14 13:05:47 -0500

    copy code methods and added two private methods addHead and assignNode

--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,35 @@
 /*
- * your comment header here
+ * COSC 2436
+ * Zachary Hall
+ * Linked Lists Assignment 
+ * 10/4/23
  */
+ 
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "data.h"
+#include "testdata.h"
+#include "iostream"
+
+class LinkedList {
+
+public:
+	
+    LinkedList();
+    ~LinkedList();
+    int getCount();
+    bool addNode(int, string*);
+    bool deleteNode(int);
+    bool getNode(int, Data*);
+    bool clearList();
+    bool exists(int);
+    void printList(bool = false);
+    
+private:
+    Node *head;
+    bool addHead(Node*, Node*);
+    void assignNode(int, string*,Node* );
+};
+
+#endif 
